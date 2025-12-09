6bbd30d6d9ea918c58bfd39678b64ff1740cf54a
joao.rodrigues <joaovr724@gmail.com>
2025-12-09 15:01:58 -0300

    Bloqueei o cadastro de datas posteriores ao dia atual e configurei o CORS para usar allowedOrigin como variável de ambiente

--- src/main/java/com/nozama/aluguel_veiculos/config/SecurityConfig.java
+++ src/main/java/com/nozama/aluguel_veiculos/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nozama.aluguel_veiculos.config;
 
 import com.nozama.aluguel_veiculos.security.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,9 @@ public class SecurityConfig {
 
     @Autowired
     private JwtFilter jwtFilter;
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
